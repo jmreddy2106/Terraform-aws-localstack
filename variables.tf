65051df1a3c306f60c111712af195a9162b9fa2b
jmreddy <jmreddy2106>
2023-07-13 16:58:10 -0400

    added aws infra variables in terraform

---
+++ variables.tf
@@ -0,0 +1,14 @@
+# Declaration of aws infra variables
+variable aws_access_key {}
+variable aws_secret_key {}
+variable aws_region {}
+variable aws_end_point {}
+variable aws_vpc_cidr {}
+variable public_subnet1_name {}
+variable public_subnet2_name {}
+variable public_subnet3_name {}
+variable public_subnet1_cidr {}
+variable public_subnet2_cidr {}
+variable public_subnet3_cidr {}
+variable aws_subnet_region {}
+variable aws_bucket_name {}
